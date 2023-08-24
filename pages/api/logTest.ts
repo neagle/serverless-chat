cfeb6a4d1d322a11883e07f63253c50345030209
Nate Eagle <nate@nateeagle.com>
2023-08-24 14:08:32 -0400

    add some additional tests

--- pages/api/logTest.ts
+++ pages/api/logTest.ts
@@ -2,5 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   console.log("This console log should be accessible in the logs!");
-  res.status(200).json({ status: "I just logged something to the console!" });
+  console.log("How do you handle multiple logs?");
+  console.log(`Oh, I love loggin', yes I do...`);
+  res.status(200).json({
+    status: "I just logged a few things to the console!",
+  });
 };
