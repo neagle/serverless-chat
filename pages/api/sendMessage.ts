e786f4b2ade4649b45161ee95347fa3eececd3fc
Nate Eagle <nate@nateeagle.com>
2023-08-24 13:32:20 -0400

    try installing optional dependencies

--- pages/api/sendMessage.ts
+++ pages/api/sendMessage.ts
@@ -4,8 +4,12 @@ import { broadcastMessage } from "./messages";
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const { username, message } = req.body;
   if (req.method === "POST" && typeof username && typeof message === "string") {
-    broadcastMessage({ username: username, text: message });
-    res.status(200).json({ status: "Message sent" });
+    try {
+      broadcastMessage({ username: username, text: message });
+      res.status(200).json({ status: "Message sent" });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
   } else {
     res.status(400).json({ error: "Bad request" });
   }
